c625ee7e8b53dbfd391fa09365f63945f395795e
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-07-17 12:37:20 +0530

    Updated Client Side

--- module-02/src/ClientSide.java
+++ module-02/src/ClientSide.java
@@ -14,8 +14,8 @@ public class ClientSide {
 
     public static void main(String[] args) throws IOException {
         final int PORT = 8000;
-        ServerSocket serverSocket = new ServerSocket(PORT);
-        System.out.println("Server is up and running on port : "+PORT);
-        Socket socket = serverSocket.accept();
+        Socket socket = new Socket("localhost", PORT);
+
+
     }
 }
